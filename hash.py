1a0856c5c3e3a053387c8bffc73c11e1255dcaa1
javad pournosrat <pournosrat.javad@gmail.com>
2021-03-30 02:04:04 +0430

    add user input string

--- hash.py
+++ hash.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3.9
+
 import hashlib
+string = input(str('enter your string:'))
 
 def sha256():
-    Sha256 = hashlib.sha256(b'string').hexdigest()
+    Sha256 = hashlib.sha256(string.encode()).hexdigest()
     print(f'Sha256:{Sha256}')
 sha256()
 def md5():
-    Md5 = hashlib.md5(b'string').hexdigest()
+    Md5 = hashlib.md5(string.encode()).hexdigest()
     print(f'Md5:{Md5}')
 md5()
 def sha224():
-    Sha224 = hashlib.sha224(b'string').hexdigest()
+    Sha224 = hashlib.sha224(string.encode()).hexdigest()
     print(f'Sha224:{Sha224}')
 sha224()
 def sha1():
-    sha1 = hashlib.sha1(b'string').hexdigest()
+    sha1 = hashlib.sha1(string.encode()).hexdigest()
     print(f'sha1:{sha1}')
 sha1()
